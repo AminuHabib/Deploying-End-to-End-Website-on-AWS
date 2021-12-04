a5bcea19c543e1766c87c7d2fa8b28025d91b67b
Aminu Habib Abiola <habib.aminu@edu.dsti.institute>
2021-12-04 03:39:28 +0100

    update config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
-customuser = "intellipaat"
-custompass = "intellipaat123"
+customuser = "habib"
+custompass = "test123"
 customdb = "employee"
 custombucket = "addemployee"
 customregion = "us-east-2"
